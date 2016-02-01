88f3f9abce680437eb1f056fdf3d035c6628db72
zhangmaoqiang zhangmaoqiang <zhangmaoqiang@cq-web-fe-ugc-test01.cq.sankuai.com>
2016-02-01 11:59:37 +0800

    change init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
-mv * ~/.vim/.
-echo source .vim/.vimrc >> ~/.vimrc
+if !(test -d ~/.vim/colors)
+then
+    mkdir -p ~/.vim/colors
+    echo haha
+fi
+cp -r * ~/.vim/.
+user=$(whoami)
+if !(test -e ~/.vimrc)
+then
+	echo source ~/.vim/vimrc >> ~/.vimrc
+else
+	if !(grep -Fxq "source /home/$user/.vim/vimrc" ~/.vimrc)
+	then
+		echo source ~/.vim/vimrc >> ~/.vimrc
+	fi
+fi
